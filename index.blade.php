20f46790977fd964b16a69369036cc3a990a2f87
Yuya <yuya08420bs@gmail.com>
2024-12-30 04:53:02 +0900

    写真の追加&コンポーネント作成

---
+++ index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Portfolio - Yuya\'s Lab')
+@section('content')
+    <div id="app">
+        <Portfolio img-path="{{ asset('images/portfolio/BackGroundImage.png') }}"></Portfolio>
+    </div>
+@endsection
