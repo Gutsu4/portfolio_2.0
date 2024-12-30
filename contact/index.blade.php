d6037b7c00a63733e0cf7b562ae5b6ffbe4eecd4
Yuya <yuya08420bs@gmail.com>
2024-12-31 04:32:51 +0900

    project page作成

---
+++ contact/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Contact - Yuya\'s Lab')
+@section('content')
+    <div id="app">
+        <Contact img-path="{{ asset('images/portfolio/BackGroundImage.png') }}"></Contact>
+    </div>
+@endsection
