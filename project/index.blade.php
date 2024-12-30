d7b6d1ced3f0732e54f7a857cefeef781e8308f8
Yuya <yuya08420bs@gmail.com>
2024-12-31 04:17:49 +0900

    project page作成

---
+++ project/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Project - Yuya\'s Lab')
+@section('content')
+    <div id="app">
+        <Project img-path="{{ asset('images/portfolio/BackGroundImage.png') }}"></Project>
+    </div>
+@endsection
